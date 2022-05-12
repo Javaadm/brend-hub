cf16f9239a2dfe58add39db30ad3925345968412
ImHantu <dk.public.use@gmail.com>
2022-05-12 17:05:42 +0300

    + metrika @ services

--- resources/views/services/brandbook.php
+++ resources/views/services/brandbook.php
@@ -18,6 +18,12 @@
 </head>
 
 <body>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(51242554, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/51242554" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+    <div class="wrapper wrapper_fixed">
+        <!--=======================================================================================-->
+        
     <div class="wrapper">
         <!--=======================================================================================-->
         <header class="main-header">
