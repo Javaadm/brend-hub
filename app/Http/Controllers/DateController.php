e3ded4f2073329601c55e64155b79a5c049df5a9
javaAdm <s.ziryanov@pos-credit.ru>
2022-05-18 18:15:05 +0700

    add lead and analitica

--- app/Http/Controllers/DateController.php
+++ app/Http/Controllers/DateController.php
@@ -10,18 +10,18 @@ use Illuminate\Support\Facades\Log;
 class DateController extends Controller
 {
     public const CONST_DESS_ARRAY = [
-        ["title"=>"Сфера деятельности","body"=>"Это то, что помогает нам оставаться в индустрии, соответствовать ей. Оно не уникальное, а скорее это общая канва для двух."],
-        ["title"=>"Социальная активность","body"=>"Продукты, связанные  с поддержанием социально незащищенных групп: биоразлагающаяся упаковка, помогающий пожилым продукт."],
-        ["title"=>"Материальные активы","body"=>"Физическое имущество: земля, здания, машины/станки и прочее. Помогает отличаться от конкурентов."],
-        ["title"=>"Марткетинг и PR","body"=>"Сильный маркетинг, коллаборации, амбасадоры бренда, лицо бренда - медийная личность и т.д."],
-        ["title"=>"Награды и заслуги","body"=>"Обладатель наград, премий, лауреат конкурсов в своей сфере, общественное признание и т.д."],
-        ["title"=>"Дизайн и интерьер","body"=>"Аутентичный интерьер. Обычно подходит для ресторанов и кафе, а так же сфере развлечений."],
-        ["title"=>"Продукт","body"=>"Продуктовое предложение, которое выделяется на рынке. Это может быть шоколад, который не тает  в ладони."],
-        ["title"=>"Ассортимент","body"=>"Выбор на все случаи жизни, или, наоборот, редкие позиции под небольшой сегмент аудитории."],
-        ["title"=>"Сырье и состав","body"=>"Особые компоненты в составе. Возможно, товар на 100% органичный или с добавлением масла гуараны."],
-        ["title"=>"Страна происхождения","body"=>"Ваш знак качества: произведено в Германии, пошито в Италии или designed in California."],
-        ["title"=>"Технологии и инновации","body"=>"Уникальные технологии.  Например итальянская производственная линия для пекарен, высокоточное оборудование в медицине."],
-        ["title"=>"Персонал","body"=>"В вашей команде крутые эксперты. Они известны на рынке и за его пределами."],
+        ["title"=>"Сфера деятельности","ymGoal"=>"brief-company-details-click-industry","body"=>"Это то, что помогает нам оставаться в индустрии, соответствовать ей. Оно не уникальное, а скорее это общая канва для двух."],
+        ["title"=>"Социальная активность","ymGoal"=>"brief-company-details-click-socialproject","body"=>"Продукты, связанные  с поддержанием социально незащищенных групп: биоразлагающаяся упаковка, помогающий пожилым продукт."],
+        ["title"=>"Материальные активы","ymGoal"=>"brief-company-details-click-assets","body"=>"Физическое имущество: земля, здания, машины/станки и прочее. Помогает отличаться от конкурентов."],
+        ["title"=>"Марткетинг и PR","ymGoal"=>"brief-company-details-click-marketing","body"=>"Сильный маркетинг, коллаборации, амбасадоры бренда, лицо бренда - медийная личность и т.д."],
+        ["title"=>"Награды и заслуги","ymGoal"=>"brief-company-details-click-achievements","body"=>"Обладатель наград, премий, лауреат конкурсов в своей сфере, общественное признание и т.д."],
+        ["title"=>"Дизайн и интерьер","ymGoal"=>"brief-company-details-click-design","body"=>"Аутентичный интерьер. Обычно подходит для ресторанов и кафе, а так же сфере развлечений."],
+        ["title"=>"Продукт","ymGoal"=>"brief-company-details-click-product","body"=>"Продуктовое предложение, которое выделяется на рынке. Это может быть шоколад, который не тает  в ладони."],
+        ["title"=>"Ассортимент","ymGoal"=>"brief-company-details-click-productrange","body"=>"Выбор на все случаи жизни, или, наоборот, редкие позиции под небольшой сегмент аудитории."],
+        ["title"=>"Сырье и состав","ymGoal"=>"brief-company-details-click-ingredients","body"=>"Особые компоненты в составе. Возможно, товар на 100% органичный или с добавлением масла гуараны."],
+        ["title"=>"Страна происхождения","ymGoal"=>"brief-company-details-click-country","body"=>"Ваш знак качества: произведено в Германии, пошито в Италии или designed in California."],
+        ["title"=>"Технологии и инновации","ymGoal"=>"brief-company-details-click-innovations","body"=>"Уникальные технологии.  Например итальянская производственная линия для пекарен, высокоточное оборудование в медицине."],
+        ["title"=>"Персонал","ymGoal"=>"brief-company-details-click-people","body"=>"В вашей команде крутые эксперты. Они известны на рынке и за его пределами."],
     ];
 
     public const CONST_EMOTIONS_VALUE = [
@@ -43,8 +43,101 @@ class DateController extends Controller
     "Дорогой",
     "Новаторский",
     ];
+
+    public const CONST_BOARD_V2_CARD_COLOR_VALUE = [
+        "Контрастный"=>["#FBCE01","#B44100","#1D1318","#1CA4D2","#0D77A5"],
+        "Винтажный"=>['#BFB7A7', '#4B473F', '#A58B6C', '#827F67', '#8A8D74'],
+        "Монохромный"=>['#042F45', '#055F88', '#4A94AD', '#79ACBD', '#1A7B9E'],
+        "Digital"=>['#8982C7', '#C170C6', '#5B5178', '#5051AC', '#5051AC'],
+        "Чистый"=>['#D6DFE1', '#9AB4C0', '#66848E', '#858D8C', '#7C797C'],
+        "Цветной"=>['#8DAE59', '#00B4C5', '#61449F', '#E40204', '#FEC300'],
+        "Черно-белый"=>['#CCCCCC', '#212121', '#999999', '#727272', '#8C8C8C'],
+        "Неоновый"=>['#594784', '#E94985', '#49CDA7', '#4F1838', '#B294BC'],
+        "Экологичный"=>['#CBC0B8', '#3F4235', '#5F6D44', '#898863', '#606658']
+    ];
+
+    public const CONST_BOARD_V2_CARD_PATTERN_VALUE = [
+        "Крафт",
+        "Паттерн",
+        "Этно",
+        "Гламур",
+        "Люкс",
+        "Реализм",
+        "Эко",
+        "Рисунок",
+        "Motion"
+    ];
+
+    public const CONST_BOARD_V2_CARD_WORDS_VALUE = [
+        "Радостный",
+        "Дружелюбный",
+        "Нейтральный",
+        "Функциональный",
+        "Надежный",
+        "Уверенный",
+        "Настойчивый",
+        "Гедонистичный",
+        "Бюджетный",
+        "Консервативный",
+        "Современный",
+        "Лаконичный",
+        "Элегантный",
+        "Ободряющий",
+        "Дерзкий",
+        "Дорогой",
+        "Новаторский"
+    ];
+
+    private const YM_GOALS_FOR_PRODUCT_REFERENCES = [
+        // first page (keys: 0-9)
+        'brief-mood-package-click-bahroma',
+        'brief-mood-package-click-lemonade',
+        'brief-mood-package-click-sushi',
+        'brief-mood-package-click-orangemix',
+        'brief-mood-package-click-spicemode',
+        'brief-mood-package-click-veggieburger',
+        'brief-mood-package-click-peas',
+        'brief-mood-package-click-siggis',
+        'brief-mood-package-click-apetina',
+        // second page (keys: 9-18)
+        'brief-mood-package-click-gotham',
+        'brief-mood-package-click-tetapizza',
+        'brief-mood-package-click-sausages',
+        'brief-mood-package-click-dovesfarm',
+        'brief-mood-package-click-americanpicnic',
+        'brief-mood-package-click-cereals',
+        'brief-mood-package-click-smalls',
+        'brief-mood-package-click-joecoffee',
+        'brief-mood-package-click-tanzania',
+    ];
+
+    private const YM_GOALS_FOR_SERVICE_REFERENCES = [
+        // first page (keys: 0-9)
+        'brief-mood-service-click-turbo',
+        'brief-mood-service-click-heringfoundation',
+        'brief-mood-service-click-vivahjewels',
+        'brief-mood-service-click-oloo',
+        'brief-mood-service-click-mauracora',
+        'brief-mood-service-click-skatra',
+        'brief-mood-service-click-harone',
+        'brief-mood-service-click-moon',
+        'brief-mood-service-click-angelsandco',
+        // second page (keys: 9-18)
+        'brief-mood-service-click-olivequeen',
+        'brief-mood-service-click-doglove',
+        'brief-mood-service-click-devine',
+        'brief-mood-service-click-lesliesmith',
+        'brief-mood-service-click-childrenspublishing',
+        'brief-mood-service-click-broomtail',
+        'brief-mood-service-click-lildingus',
+        'brief-mood-service-click-dreamer',
+        'brief-mood-service-click-rachelmovits',
+    ];
+
     public function render(Request $request)
     {
+
+
         $session = session();
 
         if(!$request->session()->has("type")) {
@@ -54,6 +147,7 @@ class DateController extends Controller
         if(!$request->session()->has("step-1-board")) {
             $request->session()->put("step-1-board", "1");
         }
+
         if(!$request->session()->has("tariff")) {
             $request->session()->put("tariff", "1");
         }
@@ -81,14 +175,75 @@ class DateController extends Controller
 
         }
 
+        if(!$request->session()->has("images-v2-color")) {
+            $elements = [];
+            for ($i=0;$i<9;$i++){
+                $elements[] = ["isSelected" => false];
+            }
+            $request->session()->put("images-v2-color", $elements);
+        }
+
+        if(!$request->session()->has("images-v2-shape")) {
+            $elements = [];
+            for ($i=0;$i<6;$i++){
+                $elements[] = ["isSelected" => false];
+            }
+            $request->session()->put("images-v2-shape", $elements);
+        }
+
+        if(!$request->session()->has("images-v2-pattern")) {
+            $elements = [];
+            for ($i=0;$i<9;$i++){
+                $elements[] = ["isSelected" => false];
+            }
+            $request->session()->put("images-v2-pattern", $elements);
+        }
+
+        if(!$request->session()->has("images-v2-font")) {
+            $elements = [];
+            for ($i=0;$i<9;$i++){
+                $elements[] = ["isSelected" => false];
+            }
+            $request->session()->put("images-v2-font", $elements);
+        }
+
+        if(!$request->session()->has("words-v2")) {
+            $elements = [];
+            for ($i=0;$i<17;$i++){
+                $elements[] = ["isSelected" => false];
+            }
+            $request->session()->put("words-v2", $elements);
+        }
+
+        $board = "v1";
 
 
-         return view('index',
-                     [
-                         "dessArray"=>self::CONST_DESS_ARRAY,
-                         "emotionsValue"=>self::CONST_EMOTIONS_VALUE
-                     ]);
+        if($request->get("board")){
+            $board = $request->get("board");
+            $request->session()->put("board", $board);
+        }else if(!$request->session()->has("board")){
+            $request->session()->put("board", $board);
+        }else{
+            $board = $request->session()->get("board");
+        }
+
+
+        return view(
+            "main-board.board-v1",
+            [
+                "dessArray" => self::CONST_DESS_ARRAY,
+                "emotionsValue" => self::CONST_EMOTIONS_VALUE,
+                "colorsValue" => self::CONST_BOARD_V2_CARD_COLOR_VALUE,
+                "patternsValue" => self::CONST_BOARD_V2_CARD_PATTERN_VALUE,
+                "wordsValue" => self::CONST_BOARD_V2_CARD_WORDS_VALUE,
+                'ymGoalsForProductReferences' => self::YM_GOALS_FOR_PRODUCT_REFERENCES,
+                'ymGoalsForServiceReferences' => self::YM_GOALS_FOR_SERVICE_REFERENCES,
+            ]
+        );
+
     }
+
+
 
     public function updateSession(Request $request)
     {
@@ -101,6 +256,15 @@ class DateController extends Controller
             $number = $request->request->get("image");
 
             $images = $request->session()->get("images");
+
+            $images[$number]["isSelected"] = !$images[$number]["isSelected"];
+            $request->session()->put("images", $images);
+        }
+
+        if ($request->request->has("image-v2")) {
+            $number = $request->request->get("image-v2");
+
+            $images = $request->session()->get("images-v2");
 
             $images[$number]["isSelected"] = !$images[$number]["isSelected"];
             $request->session()->put("images", $images);
@@ -188,6 +352,39 @@ class DateController extends Controller
 
             $request->session()->put("images", $images);
         }
+
+        if ($request->request->has('images-v2-color')){
+            $number = $request->request->get("images-v2-color");
+            $images = $request->session()->get("images-v2-color");
+            $images[$number]["isSelected"] = !$images[$number]["isSelected"];
+            $request->session()->put("images-v2-color", $images);
+
+        }
+
+        if ($request->request->has('images-v2-shape')){
+            $number = $request->request->get("images-v2-shape");
+            $images = $request->session()->get("images-v2-shape");
+            $images[$number]["isSelected"] = !$images[$number]["isSelected"];
+            $request->session()->put("images-v2-shape", $images);
+
+        }
+
+        if ($request->request->has('images-v2-pattern')){
+            $number = $request->request->get("images-v2-pattern");
+            $images = $request->session()->get("images-v2-pattern");
+            $images[$number]["isSelected"] = !$images[$number]["isSelected"];
+            $request->session()->put("images-v2-pattern", $images);
+
+        }
+
+        if ($request->request->has('images-v2-font')){
+            $number = $request->request->get("images-v2-font");
+            $images = $request->session()->get("images-v2-font");
+            $images[$number]["isSelected"] = !$images[$number]["isSelected"];
+            $request->session()->put("images-v2-font", $images);
+
+        }
+
     }
 
     public function genPDF(){
@@ -210,7 +407,6 @@ class DateController extends Controller
     }
     public function showSession(Request $request)
     {
-
         dd($request->session()->all());
     }
 }
